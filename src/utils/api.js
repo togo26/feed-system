aa82cf65c04ba39080463050cf58b0b47a39a5ec
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2021-01-31 23:12:13 +0900

    feat: add api for getting ad banner list

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,19 @@
+const baseUrl = "https://problem.comento.kr/api";
+const options = {
+  headers: {
+    Accept: "application/json"
+  }
+};
+
 export const fetchFeedList = async () => {
   const result = await fetch(
-    "https://problem.comento.kr/api/list?page=1&ord=asc&limit=5&category%5B%5D=1",
-    {
-      headers: {
-        Accept: "application/json"
-      }
-    }
+    `${baseUrl}/list?page=1&ord=asc&limit=10&category%5B%5D=1`,
+    options
   );
+  return result.json();
+};
+
+export const fetchAdBannerList = async () => {
+  const result = await fetch(`${baseUrl}/ads?page=1&limit=4`, options);
   return result.json();
 };

a9ccf27759c81151f9868c5e73b4df5efb418f24
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2021-01-31 23:18:38 +0900

    feat: add Card component for conditional rendering

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <article>
+    <feed-card v-if="(index + 1) % 4" :feed="content" />
+    <ad-card v-else :ad="content" />
+  </article>
+</template>
+
+<script>
+import FeedCard from "@/components/FeedCard.vue";
+import AdCard from "@/components/AdCard.vue";
+
+export default {
+  props: ["content", "index"],
+  components: {
+    FeedCard,
+    AdCard
+  }
+};
+</script>

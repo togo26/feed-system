19a67ecf35dfeaf104f37d0d24843a376e9c8159
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2021-01-31 17:49:55 +0900

    feat: add FeedCard, FeedList components

---
+++ src/components/FeedList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <feed-card :key="i" :feed="item" v-for="(item, i) in feedList" />
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import FeedCard from "@/components/FeedCard.vue";
+export default {
+  components: {
+    FeedCard
+  },
+  computed: {
+    ...mapState(["feedList"])
+  }
+};
+</script>

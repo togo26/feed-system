1f9c99848e2d6a3ce05a63d751c765097c9b010e
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2021-01-31 17:47:27 +0900

    feat: add mutation & action for adding feed list in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,27 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { fetchFeedList } from "../utils/api.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    feedList: []
+  },
+  mutations: {
+    addFeedList(state, payload) {
+      state.feedList = [...state.feedList, ...payload];
+    }
+  },
+  actions: {
+    async addFeedList({ commit }) {
+      try {
+        const result = await fetchFeedList();
+        commit("addFeedList", result.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
   modules: {}
 });

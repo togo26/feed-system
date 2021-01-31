f2f567e2989d4ea0f7eac8b27074dc99eda43f8e
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2021-01-31 17:51:24 +0900

    feat: update Home component for feed list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,72 @@
 <template>
   <div class="home">
-    <h1>This is home</h1>
+    <div class="wrapper">
+      <aside>
+        <Button text="로그인" />
+      </aside>
+      <section>
+        <div class="sorting-controls">
+          <order-by-controls />
+          <filter-button text="필터" />
+        </div>
+        <feed-list />
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+import Button from "@/components/Button.vue";
+import OrderByControls from "@/components/OrderByControls.vue";
+import FilterButton from "@/components/FilterButton.vue";
+import FeedList from "@/components/FeedList.vue";
+
 export default {
   name: "Home",
-  components: {}
+  components: {
+    Button,
+    FilterButton,
+    FeedList,
+    OrderByControls
+  },
+  methods: {
+    ...mapActions(["addFeedList"])
+  },
+  async beforeMount() {
+    await this.addFeedList();
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+}
+
+.wrapper {
+  display: flex;
+  justify-content: center;
+  width: 80%;
+  max-width: 1140px;
+}
+
+aside {
+  width: 235px;
+  margin-right: 40px;
+}
+
+section {
+  width: 865px;
+}
+
+.sorting-controls {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 24px;
+  margin-bottom: 11px;
+}
+</style>

397143c45953b63a206847254bded2bf42ece493
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2021-02-01 22:48:36 +0900

    feat: add FeedDetailCard, ReplayCard components

---
+++ src/components/FeedDetailCard.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="wrapper">
+    <h3>{{ feed.title }}</h3>
+    <p>{{ feed.contents }}</p>
+    <p class="created-at">{{ sliceTimeString(feed.created_at) }}</p>
+  </div>
+</template>
+
+<script>
+import { sliceTimeString } from "@/utils/sliceTimeString.js";
+export default {
+  props: ["feed"],
+  data() {
+    return {
+      sliceTimeString
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  box-sizing: border-box;
+  width: 100%;
+  height: 210px;
+  margin-bottom: 30px;
+  padding: 25px 30px;
+  border-radius: 5px;
+  border: solid 1px #00c854;
+}
+
+h3 {
+  width: 96%;
+  height: 40px;
+  margin-bottom: 14px;
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 22px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: initial;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+}
+
+p {
+  width: 96%;
+  height: 72px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  line-height: 26px;
+  color: #495057;
+  margin-bottom: 14px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: initial;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+}
+
+.created-at {
+  font-size: 13px;
+  color: #adb5bd;
+}
+</style>

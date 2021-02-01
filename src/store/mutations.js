63b12dddfb5eda0d98df1c7fd0216e2d00746bc2
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2021-02-02 03:44:59 +0900

    refactor: separate store file into mutation and action files

---
+++ src/store/mutations.js
@@ -0,0 +1,71 @@
+export default {
+  addFeedList(state, payload) {
+    const mapCategoryNameToList = payload.list.map(feed => ({
+      ...feed,
+      category_id:
+        state.categories[feed.category_id - 1]?.name || feed.category_id
+    }));
+    state.feedList = [...state.feedList, ...mapCategoryNameToList];
+  },
+  addAdBannerList(state, payload) {
+    state.adBannerList = [...state.adBannerList, ...payload.list];
+  },
+  addFeedListWithAdBanners(state) {
+    const { feedList, adBannerList } = state;
+    const combinedList = [];
+    let lastFeedGroupIndex = 0;
+
+    for (let i = 0, j = 0; i < feedList.length; i += 2, j += 3) {
+      const feeds = feedList.slice(j, j + 3);
+      if (feeds.length <= 0) {
+        lastFeedGroupIndex = i - 2;
+        break;
+      }
+      combinedList[i] = feeds;
+    }
+
+    for (let i = 0; i < combinedList.length; i++) {
+      const targetIndex = (i + 1) * 2 - 1;
+      const lastFeedGroupLength = combinedList[targetIndex - 1]?.length;
+
+      if (lastFeedGroupLength < 3) break;
+      if (lastFeedGroupIndex < targetIndex) {
+        if (lastFeedGroupLength >= 3) {
+          combinedList[targetIndex] = adBannerList[i] || false;
+          break;
+        }
+      }
+      combinedList[targetIndex] = adBannerList[i] || false;
+    }
+
+    state.contentList = combinedList.flat();
+  },
+  deleteAllList(state) {
+    state.feedList = [];
+    state.adBannerList = [];
+    state.contentList = [];
+    state.lastPage = 0;
+    state.currentPage = 1;
+  },
+  setLastPage(state, payload) {
+    state.lastPage = payload.lastPage;
+  },
+  updateCurrentPage(state) {
+    state.currentPage++;
+  },
+  updateOrderBy(state, payload) {
+    state.orderBy = payload.orderBy;
+  },
+  resetOrderBy(state) {
+    state.orderBy = "ascending";
+  },
+  addCategories(state, payload) {
+    state.categories = payload.list.map(category => ({
+      ...category,
+      isChecked: true
+    }));
+  },
+  updateCategories(state, payload) {
+    state.categories = payload.list;
+  }
+};

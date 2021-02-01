42f8a584a530ac029b71670888edffcf52e30541
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2021-02-01 19:13:20 +0900

    feat: add CategoryFilter, CheckBox components

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <label>
+    <input
+      type="checkbox"
+      @click="e => $emit('handle-checkbox-click', e.target)"
+      :id="category.id"
+      :checked="category.isChecked"
+    />
+    {{ category.name }}
+  </label>
+</template>
+
+<script>
+export default {
+  props: ["category"]
+};
+</script>
